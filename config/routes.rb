6338965df946d29514e5d7852b075b4eb99bda7c
uma vadlamani <umasai.vadlamani@kellogg.northwestern.edu>
2024-02-08 03:34:40 +0000

    Complete Class 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  get "/tacos", :controller => "tacos", :action => "index"
+  get "/dice", :controller => "dice", :action => "index"
+  resources "companies"
+  resources "contacts"
+  #does the same thing as the get above, its just faster
 end
